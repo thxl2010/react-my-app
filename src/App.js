eafe513d49296f0e014719faad4c8149a13cebcf
Duyb <1255248666@qq.com>
2020-05-31 15:50:05 +0800

    Add: Portal 提供了一种将子节点渲染到存在于父组件以外的 DOM 节点的优秀的方案

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Button } from 'antd';
 // import React from 'react';
 import React, { useState, useEffect } from 'react'; // Hook
+import ReactDOM from 'react-dom';
 import './App.css';
 import Chosen from './components/Chosen';
 import logo from './logo.svg';
@@ -1147,6 +1148,86 @@ class MouseTracker2 extends React.Component {
 }
 
 /**
+ * Portals
+ * Portal 提供了一种将子节点渲染到存在于父组件以外的 DOM 节点的优秀的方案
+ */
+const modalRoot = document.getElementById('modal-root');
+class Modal extends React.Component {
+  constructor(props) {
+    super(props);
+    this.el = document.createElement('div');
+  }
+
+  componentDidMount() {
+    // 在 Modal 的所有子元素被挂载后，
+    // 这个 portal 元素会被嵌入到 DOM 树中，
+    // 这意味着子元素将被挂载到一个分离的 DOM 节点中。
+    // 如果要求子组件在挂载时可以立刻接入 DOM 树，
+    // 例如衡量一个 DOM 节点，
+    // 或者在后代节点中使用 ‘autoFocus’，
+    // 则需添加 state 到 Modal 中，
+    // 仅当 Modal 被插入 DOM 树中才能渲染子元素。
+    modalRoot.appendChild(this.el);
+  }
+
+  componentWillUnmount() {
+    modalRoot.removeChild(this.el);
+  }
+
+  render() {
+    return ReactDOM.createPortal(
+      this.props.children,
+      this.el
+    );
+  }
+}
+
+class Parent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {clicks: 0};
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    // 当子元素里的按钮被点击时，
+    // 这个将会被触发更新父元素的 state，
+    // 即使这个按钮在 DOM 中不是直接关联的后代
+    this.setState(state => ({
+      clicks: state.clicks + 1
+    }));
+  }
+
+  render() {
+    return (
+      <div onClick={this.handleClick}>
+        <p>Number of clicks: {this.state.clicks}</p>
+        <p>
+          Open up the browser DevTools
+          to observe that the button
+          is not a child of the div
+          with the onClick handler.
+        </p>
+        <Modal>
+          <Child />
+        </Modal>
+      </div>
+    );
+  }
+}
+
+function Child() {
+  // 这个按钮的点击事件会冒泡到父元素
+  // 因为这里没有定义 'onClick' 属性
+  return (
+    <div className="modal">
+      <button>Click  Portal</button>
+      <button>hide</button>
+    </div>
+  );
+}
+
+/**
  * ========
  * [Hook](https://zh-hans.reactjs.org/docs/hooks-intro.html)
  * ========
@@ -1287,6 +1368,9 @@ function App() {
         <HelloNoJSX toWhat={'Du'} />
         <h2>Render Props： 》》》</h2>
         <MouseTracker />
+
+        <h2>Portal 》》》 提供了一种将子节点渲染到存在于父组件以外的 DOM 节点的优秀的方案</h2>
+        <Parent />
 
         <h2>
           Hook： 》》》 Hook 是 React 16.8 的新增特性。它可以让你在不编写 class
