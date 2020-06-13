17f2773220c1c08c675079b5c9424dd01f2132be
Duyb <1255248666@qq.com>
2020-06-13 16:59:29 +0800

    chore: antd theme

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import store from './redux/store';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import '@/assets/styles/index.less';
 
 ReactDOM.render(
   <Provider store={store}>
