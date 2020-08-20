9a2b49d6d2a5ca916f1e13a8027702a373974b4e
Duyb <1255248666@qq.com>
2020-08-20 15:19:18 +0800

    Add: antd 国际化

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
+import { ConfigProvider } from 'antd';
+import zhCN from 'antd/es/locale/zh_CN';
+import moment from 'moment';
+import 'moment/locale/zh-cn';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import store from './redux/store';
-import './index.css';
 import App from './App';
+import './index.css';
+import store from './redux/store';
 import * as serviceWorker from './serviceWorker';
 
+moment.locale('zh-cn');
+
 ReactDOM.render(
-  <Provider store={store}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </Provider>,
+  <ConfigProvider locale={zhCN}>
+    <Provider store={store}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </Provider>
+  </ConfigProvider>,
   document.getElementById('root')
 );
 
--- src/views/Todo/index.js
+++ src/views/Todo/index.js
@@ -1,12 +1,18 @@
+import { DatePicker } from 'antd';
 import React from 'react';
 import AddTodo from './components/AddTodo';
 import TodoList from './components/TodoList';
 import VisibilityFilters from './components/VisibilityFilters';
 import './styles.css';
 
+function onChange(date, dateString) {
+  console.log(date, dateString);
+}
+
 export default function TodoApp() {
   return (
     <div className="todo-app">
+      <DatePicker onChange={onChange} />
       <h1>Todo List</h1>
       <AddTodo />
       <TodoList />
